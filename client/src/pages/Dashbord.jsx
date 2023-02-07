e908ad39ff4c7e0a74033499832ec4e955a6c17b
Hashimjasem <you@example.com>
2023-02-07 17:29:44 +0800

    changed routes back to old format

--- client/src/pages/Dashbord.jsx
+++ client/src/pages/Dashbord.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Dashbord() {
-  return (
-    <div>Dashbord</div>
-  )
+  const navigate = useNavigate();
+
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
+  return <div>Dashbord</div>;
 }
 
-export default Dashbord
+export default Dashbord;
